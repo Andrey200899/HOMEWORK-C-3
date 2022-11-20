2276cfef381758cd440d001e09952d8ec0cd667e
unknown <kvkvachev@yandex.ru>
2022-11-20 22:06:38 +0300

    задача 21

--- Program.cs
+++ Program.cs
@@ -13,9 +13,16 @@
 //else  Console.WriteLine("Число не полиндромное");
 
 // Задача № 23
-Console.WriteLine("Введите число : ");
-int Num =int.Parse(Console.ReadLine()!);
-for (int i = 1; i<= Num ; i++)
-{
-    Console.Write($"{Math.Pow(i,3)}");
-}
+//Console.WriteLine("Введите число : ");
+//int Num =int.Parse(Console.ReadLine()!);
+///for (int i = 1; i<= Num ; i++)
+//{
+ //   Console.Write($"{Math.Pow(i,3)}");
+//}
+
+
+// Задача № 21
+//int[] mas ={3,6,8,2,1,-7};
+//int[] mas ={7,-5, 0,1,-1,9};
+//double masR = Math.Sqrt (Math.Pow(mas[0]-mas[3],2)+ Math.Pow (mas[1]-mas[4],2)+Math.Pow (mas[2]-mas[5],2));
+//Console.WriteLine($"{masR:f2}");
