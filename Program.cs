b53d8522ba3f82b393d215a7cf571de990f35ba0
unknown <kvkvachev@yandex.ru>
2022-11-20 18:44:27 +0300

    задача 23

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿// Задача № 19
 //int N = new Random().Next(10000,99999);
 //Console.WriteLine(N);
-Console.WriteLine("Введите пятизначное число : ");
-int N =int.Parse(Console.ReadLine ()!);
-string numbers =Convert.ToString(N);
-int first =Convert.ToInt32(numbers[0]);
-int second =Convert.ToInt32(numbers[1]);
-int third =Convert.ToInt32(numbers[3]);
-int four =Convert.ToInt32(numbers[4]);
-if (first==four && second == third )
-Console.WriteLine("Число полиндромное");
-else  Console.WriteLine("Число не полиндромное");
+//Console.WriteLine("Введите пятизначное число : ");
+//int N =int.Parse(Console.ReadLine ()!);
+//string numbers =Convert.ToString(N);
+//int first =Convert.ToInt32(numbers[0]);
+//int second =Convert.ToInt32(numbers[1]);
+//int third =Convert.ToInt32(numbers[3]);
+//int four =Convert.ToInt32(numbers[4]);
+//if (first==four && second == third )
+//Console.WriteLine("Число полиндромное");
+//else  Console.WriteLine("Число не полиндромное");
+
+// Задача № 23
+Console.WriteLine("Введите число : ");
+int Num =int.Parse(Console.ReadLine()!);
+for (int i = 1; i<= Num ; i++)
+{
+    Console.Write($"{Math.Pow(i,3)}");
+}
